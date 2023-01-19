2f01a94cda830af574646c7c9e4e529967072747
yl0937 <yl1033371@gmail.com>
2023-01-20 05:42:37 +0900

    default set

---
+++ src/main/java/com/example/commers/CommersApplication.java
@@ -0,0 +1,13 @@
+package com.example.commers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/commers/CommersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.commers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
